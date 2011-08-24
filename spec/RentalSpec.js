00dbd080a439201352fc3d5e6e12d6186aa51ccd
Jonathan Voigt <voigtjr@gmail.com>
2011-08-24 19:07:42 -0400

    stuff

--- spec/RentalSpec.js
+++ spec/RentalSpec.js
@@ -1,35 +1,33 @@
 describe("rental", function() {
+
+    var movie1;
+    var rental;
+    var rentalMoreDays;
+
+    beforeEach(function() {
+        movie1 = new Movie();
+        rental = new Rental(movie1);
+        rentalMoreDays = new Rental(movie1, 45);
+    });
+
 	it("has a movie", function() {
-		var rental = new Rental();
 		expect(rental.movie).toBeDefined();
 	});
-	/*it("has points", function() {
-		var type = new Type();
-		expect(type.points).toBeDefined();
-	});
-	it("regular exists", function() {
-		expect(Type.REGULAR).toBeDefined();
-	});
-	it("regular costs 25", function() {
-		var regularType = Type.REGULAR;
-		expect(regularType.cost).toBe(25);
-	});
-	it("childrens exists", function() {
-		expect(Type.CHILDRENS).toBeDefined();
-	});
-	it("childrens costs 19", function() {
-		var childrensType = Type.CHILDRENS;
-		expect(childrensType.cost).toBe(19);
-	});
-	it("new releases exists", function() {
-		expect(Type.NEW_RELEASES).toBeDefined();
-	});
-	it("new releases costs 32", function() {
-		var newReleaseType = Type.NEW_RELEASES;
-		expect(newReleaseType.cost).toBe(32);
-	});
-	it("new releases provide 39 points",function() {
-		var newReleaseType = Type.NEW_RELEASES;
-		expect(newReleaseType.points).toBe(39);
-	});*/
+    it("has days rented", function() {
+        expect(rental.days).toBeDefined();
+    });
+    it("has days rented default of 1", function() {
+        expect(rental.days).toBe(1);
+    });
+    it("has days rented be passed value", function() {
+        expect(rentalMoreDays.days).toBe(45);
+    });
+    it("has points for a one-day regular type", function() {
+        var points = rental.calcPoints();
+        expect(points).toBe(19);
+    });
+    it("has points for a 45 day regular type", function() {
+        var points = rentalMoreDays.calcPoints();
+        expect(points).toBe(19*45);
+    });
 });
--- src/Rental.js
+++ src/Rental.js
@@ -1,6 +1,11 @@
 (function(outer) {
-	outer.Rental = function() {
+	outer.Rental = function(movie, days) {
 		this.movie = movie;
+        this.days = days || 1;
 	}
+    
+    outer.Rental.prototype.calcPoints = function() {
+        return 19 * this.days;
+    }
 	
 })(this);
