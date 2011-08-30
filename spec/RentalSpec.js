60bf3c3c5226ad09b6707ad64cd3143f95b9b83d
Jonathan Voigt <voigtjr@gmail.com>
2011-08-30 09:26:42 -0400

    Updates from 8/24 meeting.

--- spec/RentalSpec.js
+++ spec/RentalSpec.js
@@ -1,33 +1,118 @@
 describe("rental", function() {
-
     var movie1;
     var rental;
-    var rentalMoreDays;
 
-    beforeEach(function() {
-        movie1 = new Movie();
-        rental = new Rental(movie1);
-        rentalMoreDays = new Rental(movie1, 45);
+    describe("1 days rental with regular movie", function() {
+        beforeEach(function() {
+            movie1 = new Movie();
+            rental = new Rental(movie1);
+        });
+
+        it("has a movie", function() {
+            expect(rental.getMovie()).toBeDefined();
+        });
+        it("has days rented", function() {
+            expect(rental.getDays()).toBeDefined();
+        });
+        it("has days rented default of 1", function() {
+            expect(rental.getDays()).toBe(1);
+        });
+        it("has points for a one-day regular type", function() {
+            var points = rental.calcPoints();
+            expect(points).toBe(23);
+        });
+        it("has cost for a one-day regular type", function() {
+            var cost = rental.calcCost();
+            expect(cost).toBe(25);
+        });
     });
 
-	it("has a movie", function() {
-		expect(rental.movie).toBeDefined();
-	});
-    it("has days rented", function() {
-        expect(rental.days).toBeDefined();
+    describe("45 day rental regular type", function() {
+        beforeEach(function() {
+            movie1 = new Movie(Type.REGULAR);
+            rental = new Rental(movie1, 45);
+        });
+        it("has days rented be passed value", function() {
+            expect(rental.getDays()).toBe(45);
+        });
+        it("has points for a 45 day regular type", function() {
+            var points = rental.calcPoints();
+            expect(points).toBe(23*45);
+        });
+        it("has cost for a 45 day regular type", function() {
+            var cost = rental.calcCost();
+            expect(cost).toBe(25*45);
+        });
     });
-    it("has days rented default of 1", function() {
-        expect(rental.days).toBe(1);
+    
+    
+    
+     describe("1 days rental with new releases movie", function() {
+        beforeEach(function() {
+            movie1 = new Movie(Type.NEW_RELEASES);
+            rental = new Rental(movie1);
+        });
+        
+        it("has points for a 1 day new releases type", function() {
+            var points = rental.calcPoints();
+            expect(points).toBe(39);
+            
+        });
+        it("has cost for a 1 day new release type", function() {
+            var cost = rental.calcCost();
+            expect(cost).toBe(32);
+        });
+
     });
-    it("has days rented be passed value", function() {
-        expect(rentalMoreDays.days).toBe(45);
+    
+    
+     describe("45 days rental with new releases movie", function() {
+        beforeEach(function() {
+            movie1 = new Movie(Type.NEW_RELEASES);
+            rental = new Rental(movie1,45);
+        });
+        
+        it("has points for a 45 day new releases type", function() {
+            var points = rental.calcPoints();
+            expect(points).toBe(39*45);
+            
+        });
+        
+        it("has cost for a 45 day new release type", function() {
+            var cost = rental.calcCost();
+            expect(cost).toBe(32 * 45);
+        });
+        
     });
-    it("has points for a one-day regular type", function() {
-        var points = rental.calcPoints();
-        expect(points).toBe(19);
+
+
+    describe("1 days rental with childrens movie", function() {
+        beforeEach(function() {
+            movie1 = new Movie(Type.CHILDRENS);
+            rental = new Rental(movie1);
+        });
+        
+        it("has points for a 1 day childrens type", function() {
+            var points = rental.calcPoints();
+            expect(points).toBe(23);
+            
+        });
+        
     });
-    it("has points for a 45 day regular type", function() {
-        var points = rentalMoreDays.calcPoints();
-        expect(points).toBe(19*45);
+    
+    describe("45 days rental with childrens movie", function() {
+        beforeEach(function() {
+            movie1 = new Movie(Type.CHILDRENS);
+            rental = new Rental(movie1, 45);
+        });
+        
+        it("has points for a 1 day childrens type", function() {
+            var points = rental.calcPoints();
+            expect(points).toBe(23*45);
+            
+        });
+        
     });
+
+    
 });
--- src/Rental.js
+++ src/Rental.js
@@ -1,11 +1,24 @@
 (function(outer) {
-	outer.Rental = function(movie, days) {
-		this.movie = movie;
-        this.days = days || 1;
+	outer.Rental = function(_movie, _days) {
+        var movie = _movie;
+        var days = _days || 1;
+        this.getMovie = function() {
+            return movie;
+        };
+        this.getDays = function() {
+            return days;
+        };
+        
 	}
     
     outer.Rental.prototype.calcPoints = function() {
-        return 19 * this.days;
+        
+        return this.getMovie().type.points  * this.getDays();
+    }
+    
+    outer.Rental.prototype.calcCost = function() {
+        
+        return this.getMovie().type.cost * this.getDays();
     }
 	
 })(this);
