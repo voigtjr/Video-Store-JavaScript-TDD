1833edf83cd6d1984ed5e7a0cabd1c43bf8b2905
Jonathan Voigt <voigtjr@gmail.com>
2011-09-09 12:18:54 -0400

    Removed x'ed test.

--- spec/MovieSpec.js
+++ spec/MovieSpec.js
@@ -4,11 +4,6 @@ describe("movies", function() {
 			var movie = new Movie();
 			expect(movie.type).toBeDefined();
 		});
-		xit("have type of type type", function() {
-			var movie = new Movie();
-			alert(movie.type.constructor);
-			expect(movie.type.constructor).toBe(Type);
-		});
 		it("should have the regular price", function() {
 			var movie = new Movie();
 			expect(movie.type).toBe(Type.REGULAR);
